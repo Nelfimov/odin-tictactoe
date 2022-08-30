f99078f681a16eb61aad7c97c89dca062e85f624
nelfimov <nelfimov@mail.ru>
2022-08-30 09:14:37 +0300

    fix draw situation

--- scripts.js
+++ scripts.js
@@ -3,6 +3,10 @@ const gameItems = {
   O: 'static/o.svg',
 }
 const checkWinningCombos = value => [
+  [[value, '-', '-'], [value, '-', '-'], [value, '-', '-']],
+  [['-', value, '-'], ['-', value, '-'], ['-', value, '-']],
+  [['-', '-', value], ['-', '-', value], ['-', '-', value]],
+
   [[value, '-', '-'], ['-', value, '-'], ['-', '-', value]],
   [['-', '-', value], ['-', value, '-'], [value, '-', '-']],
   [[value, '-', '-'], [value, '-', '-'], [value, '-', '-']],
@@ -22,18 +26,14 @@ const player1 = playerFactory(prompt('Please insert your name'));
 const player2 = playerFactory('Computer');
 
 const gameBoard = (() => {
-  let gameField = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+  let gameField = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];;
   const getGameField = () => gameField;
-  let turnSwitcher = true;
-  const getTurnSwitcher = () => turnSwitcher;
-  const changeTurnSwitcher = (value) => turnSwitcher = value;
 
   const changeAnnouncement = (string) => {
     document.getElementById('announcement').textContent = string;
   }
 
   const getPlayerScore = () => document.querySelectorAll('div.player-score>p');
-
   const getFields = () => document.querySelectorAll('div.field');
 
   const reset = () => {
@@ -42,47 +42,69 @@ const gameBoard = (() => {
       field.addEventListener('click', () => turn(field))
     });
     gameField = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
-    changeTurnSwitcher(true);
     changeAnnouncement('Play the game!');
   }
 
   const turn = field => {
     if (field.hasChildNodes()) {
       changeAnnouncement('This field is already taken!');
+      return;
     } else {
       const img = document.createElement('img');
       let classField = field.getAttribute('class').split(' ')[1];
       classField = classField.split('-');
-
-      if (getTurnSwitcher()) {
-        gameField[classField[0]][classField[1]] = 'X';
-        img.src = gameItems['X'];
-      } else {
-        gameField[classField[0]][classField[1]] = 'O';
-        img.src = gameItems['O'];
-      }
-
+      gameField[classField[0]][classField[1]] = 'X';
+      img.src = gameItems['X'];
       img.setAttribute('class', 'item');
       field.appendChild(img);
 
       if (check(gameField[classField[0]][classField[1]])) {
-        switch (getTurnSwitcher()) {
-          case true:
-            player1.win();
-            getPlayerScore()[0].textContent = player1.getScore();
-            changeAnnouncement(`${player1.getName()} wins!`)
-            break;
-          case false:
-            player2.win();
-            getPlayerScore()[0].textContent = player2.getScore();
-            changeAnnouncement(`${player2.getName()} wins!`)
-            break;
-        }
+        player1.win();
+        getPlayerScore()[0].textContent = player1.getScore();
+        changeAnnouncement(`${player1.getName()} wins!`)
         stop();
       } else {
-        changeAnnouncement('Next turn');
-        getTurnSwitcher() == true ? changeTurnSwitcher(false) : changeTurnSwitcher(true);
+        if (document.querySelectorAll('img.item').length < 9) {
+          changeAnnouncement('Next turn');
+          opponentTurn();
+        } else {
+          changeAnnouncement('Draw');
+          stop();
+        }
       }
+    }
+  }
+
+  const opponentTurn = () => {
+
+    const coordinates = () => [
+      Math.floor(Math.random() * 3),
+      Math.floor(Math.random() * 3),
+    ];
+    let field;
+    let value;
+
+
+    while (true) {
+      value = coordinates();
+      field = document
+        .querySelector(`div[class="field ${value[0]}-${value[1]}"]`);
+      if (!field.hasChildNodes()) break;
+    }
+    gameField[value[0]][value[1]] = 'O';
+
+    const img = document.createElement('img');
+    img.setAttribute('class', 'item');
+    img.src = gameItems['O'];
+    field.appendChild(img);
+
+    if (check(gameField[value[0]][value[1]])) {
+      player2.win();
+      getPlayerScore()[1].textContent = player2.getScore();
+      changeAnnouncement(`${player2.getName()} wins!`)
+      stop();
+    } else {
+      changeAnnouncement('Next turn');
     }
   }
 
@@ -96,7 +118,6 @@ const gameBoard = (() => {
       revisedGameField.forEach((combo) => {
         combo.forEach((item, index) => item != value ? combo[index] = '-' : item)
       })
-      console.log(revisedGameField);
       status = checkWinningCombos(value).some((element) =>
         JSON.stringify(element) == JSON.stringify(revisedGameField)
       );
